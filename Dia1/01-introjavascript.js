f3407e19560387b500a3859e9cd1564e3d09011d
JhanGenderson <jhan96.rc@gmail.com>
2021-04-22 22:02:43 -0500

    intro java

---
+++ Dia1/01-introjavascript.js
@@ -0,0 +1,4 @@
+// Console log nos va a servir para mostrar en la consola lo que deseemos, es util para hacer debug, o para hacernos una idea de que esta ocurriendo con nuestro codigo.
+console.log("Hola Codigo")
+
+// VARIABLES
---
+++ Dia1/03-ejercicios.js
@@ -0,0 +1,15 @@
+// // 1
+let usuario = prompt("¿Cual es tu nombre y apellido?");
+
+let saludo = "Un gusto";
+
+console.log(saludo+ " "+ `${usuario}` + ", como te va");
+
+// 2
+// let base= prompt("¿Que valor tiene la base?");
+
+// let altura= prompt("¿Que valor tiene la altura?");
+
+// console.log("Area",`${base*altura/2}`);
+
+
