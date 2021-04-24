15874e523a87f50267448c9be7430443e8ab69bd
JhanGenderson <jhan96.rc@gmail.com>
2021-04-24 14:42:04 -0500

    nuevos cambios

--- Dia2/08-while.js
+++ Dia2/08-while.js
@@ -1,12 +1,21 @@
-// While
+//while
+
 // let contador = 1;
-// while(contador <=10){
-//     console.log(`El contador es igual a 
-//     ${contador}`);
-//     contador = contador + 1;
+
+// while(contador <= 10){
+//   console.log(`El contador es igual a ${contador}`);
+//   //al final actualizamos el contador
+//   contador = contador + 1;
 // }
 
-// console.log("Yo estoy despues del while")
+// console.log("Yo estoy después del while")
 
-// do - while
+//do - while
+
 let contador2 = 1;
+
+do{
+  //dentro del do irá el código a ejecutarse
+  console.log(`El contador es igual a ${contador2}`);
+  contador2 = contador2 + 1;
+}while(contador2 <= 0)
